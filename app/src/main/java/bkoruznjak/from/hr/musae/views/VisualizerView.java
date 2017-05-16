11850bf158e4d6acce212cb7d1fd46a035f564c4
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-05-16 05:40:49 +0200

    - implemented the circular equalizer  - got a nice new color sheme

--- app/src/main/java/bkoruznjak/from/hr/musae/views/VisualizerView.java
+++ app/src/main/java/bkoruznjak/from/hr/musae/views/VisualizerView.java
@@ -4,9 +4,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RadialGradient;
 import android.graphics.Rect;
+import android.graphics.Shader;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /**
@@ -18,8 +19,14 @@ public class VisualizerView extends View {
     private byte[] mBytes;
     private float[] mPoints;
     private Rect mRect = new Rect();
-    private Paint mForePaint = new Paint();
-    private int mDivisions = 15;
+    private Paint mOddPaint = new Paint();
+    private Paint mEvenPaint = new Paint();
+    private int mDivisions = 30;
+    private float mCenterX;
+    private float mCenterY;
+    private RadialGradient magicGradient;
+    private boolean centerGrowing;
+    private int centerRadius = 120;
 
     public VisualizerView(Context context) {
         super(context);
@@ -38,9 +45,14 @@ public class VisualizerView extends View {
 
     private void init() {
         mBytes = null;
-        mForePaint.setStrokeWidth(20f);
-        mForePaint.setAntiAlias(true);
-        mForePaint.setColor(Color.rgb(0, 128, 255));
+
+        mOddPaint.setStrokeWidth(30f);
+        mOddPaint.setStrokeCap(Paint.Cap.ROUND);
+        mOddPaint.setAntiAlias(true);
+
+        mEvenPaint.setStrokeWidth(20f);
+        mEvenPaint.setStrokeCap(Paint.Cap.ROUND);
+        mEvenPaint.setAntiAlias(true);
     }
 
     public void updateVisualizer(byte[] bytes) {
@@ -49,8 +61,29 @@ public class VisualizerView extends View {
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mCenterX = getWidth() / 2;
+        mCenterY = getHeight() / 2;
+
+        int colors[] = {Color.parseColor("#A65D9E"), Color.parseColor("#E282A1"), Color.parseColor("#F6AE9B"), Color.parseColor("#F8BD9D"), Color.parseColor("#FBCA9F")};
+        magicGradient = new RadialGradient(mCenterX, mCenterY, mCenterX, colors, null, Shader.TileMode.REPEAT);
+        mOddPaint.setShader(magicGradient);
+        mEvenPaint.setShader(magicGradient);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        if (mCenterX == 0) {
+            return;
+        }
+
+        if (mCenterY == 0) {
+            return;
+        }
+
         if (mBytes == null) {
             return;
         }
@@ -72,15 +105,45 @@ public class VisualizerView extends View {
             mPoints[i * 4 + 1] = mRect.height();
             mPoints[i * 4 + 3] = mRect.height() - (dbValue * 2 - 10);
 
-            //todo use this callback to draw the points you need
-            float distance = mPoints[i*4 +1] - mPoints[i * 4 + 3];
-            Log.d("bbb","i:"+i + ", dist:"+distance);
+            float distance = mPoints[i * 4 + 1] - mPoints[i * 4 + 3];
+            float angleOffset = 360.0f / mDivisions;
+            double angle = (i * angleOffset) * Math.PI / 180;
+            if (i % 2 == 0) {
+                canvas.drawLine(mCenterX, mCenterY, getXCoordinateForKnownAngleAndDistance(mCenterX, distance, angle), getYCoordinateForKnownAngleAndDistance(mCenterY, distance, angle), mEvenPaint);
+            } else {
+                canvas.drawLine(mCenterX, mCenterY, getXCoordinateForKnownAngleAndDistance(mCenterX, distance, angle), getYCoordinateForKnownAngleAndDistance(mCenterY, distance, angle), mOddPaint);
+            }
+
         }
 
+        if (centerRadius <= 110) {
+            centerGrowing = true;
+        } else if (centerRadius >= 135) {
+            centerGrowing = false;
+        }
 
+        if (centerGrowing) {
+            centerRadius++;
+        } else {
+            centerRadius--;
+        }
 
-//        canvas.drawCircle(getWidth() /2, getHeight()/2, 50.0f, mForePaint);
-        canvas.drawLines(mPoints, mForePaint);
+        canvas.drawCircle(mCenterX, mCenterY, centerRadius, mEvenPaint);
+    }
+
+    public static float getXCoordinateForKnownAngleAndDistance(float knownX, float distance, double angle) {
+        double angleCos = Math.cos(angle);
+        float unknownX = (float) (distance * angleCos);
+        unknownX += knownX;
+        return unknownX;
+    }
+
+    public static float getYCoordinateForKnownAngleAndDistance(float knownY, float distance, double angle) {
+        double angleSin = Math.sin(angle);
+        float unknownY = (float) (distance * angleSin);
+        unknownY += knownY;
+        return unknownY;
     }
 
 }
+
